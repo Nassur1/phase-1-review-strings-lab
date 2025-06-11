db1dba827cd204f7bf5ef5c6ef8f81dd1df222ec
Nassur <nassur254@gmail.com>
2025-06-11 16:17:21 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+//Define the current user as a non-empty string
+const currentUser = 'Grace Hopper';
+
+//Create a welcome message using currentUser
+const welcomeMessage = 'Welcome to Flatbook, ${Grace Hopper}!';
+
+//Create an excited version of the welcome message in all caps
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ${GRACE HOPPER.toUpperCase()}!';
+
+//Create a short greeting using only the first initial of currentUser
+const shortGreeting = 'Welcome, ${G.slice(0,1)}!';
